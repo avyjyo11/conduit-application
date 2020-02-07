55d22103aa1d49ddfa93f1a125463000759e33aa
root <root@JYO-DELL-7559.localdomain>
2020-02-07 12:09:23 +0545

    added home-page.js

---
+++ src/pages/home-page.js
@@ -0,0 +1,13 @@
+import { LitElement } from "lit-element";
+import "../components/navigation.component";
+
+class HomePage extends LitElement {
+  static get properties() {
+    return {};
+  }
+
+  render() {
+    const navbar = html``;
+    return html``;
+  }
+}

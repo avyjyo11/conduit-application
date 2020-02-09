ac22caafef0960f6a945c27a7539353122bbcea8
roshan shrestha <roshan.shrestha4u@gmail.com>
2020-02-09 11:08:00 +0545

    design update

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import {Router} from '@vaadin/router';
 import './src/pages/signin-page';
 import './src/pages/home-page';
-import './src/pages/signup-page'
+import './src/pages/signup-page';
+import './src/pages/article-page';
 
 const outlet = document.querySelector('main');
 const router = new Router(outlet);
 router.setRoutes([
     {path:'/', component:'home-page'},
     {path:'/sign-in', component:'signin-tag'},
-    {path:'/sign-up', component:'signup-tag'}
+    {path:'/sign-up', component:'signup-tag'},
+    {path:'/publish-article', component:'article-page'},
 ]);
--- src/pages/article-page.js
+++ src/pages/article-page.js
@@ -2,12 +2,13 @@ import { LitElement, html ,css} from "lit-element";
 import "../components/navigation.component.js";
 import "../components/button.component.js";
 import "../components/input.component.js"
+import "../components/texrarea.component";
 
 class ArticlePage extends LitElement {
   static get styles(){
     return css `
       .form-container {
-        width: 50%;
+        width: 80%;
         margin: 0 auto;
       }
       .right {
@@ -28,7 +29,8 @@ class ArticlePage extends LitElement {
             <form class="form-container">
                 <input-tag placeholder="Article Title"></input-tag>
                 <input-tag placeholder="What is you article about?"></input-tag>
-                <input-tag placeholder="Write you article(in markdown)"></input-tag>
+                <textarea-tag></textarea-tag>
+                <input-tag placeholder="Enter tags"></input-tag>
                 <btn-tag buttonName="Publish Article" class="right"></btn-tag>
             </form>
     </div>
